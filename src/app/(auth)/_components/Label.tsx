af9b16cbacb090e975dabfa6a54baf53bdfd6c59
oneieo <sun020716@gmail.com>
2024-07-20 15:32:33 +0900

    [style] : (auth/sign-up) 스타일 1차 완료

--- src/app/(auth)/_components/Label.tsx
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-
-// const Label = () => {
-//   return <label>라벨</label>;
-// };
-
-// export default Label;
--- src/app/(auth)/_components/SignUp.tsx
+++ src/app/(auth)/_components/SignUp.tsx
@@ -1,36 +1,82 @@
-import React from "react";
-//import { HiOutlineMusicalNote } from "react-icons/hi2";
+"use client";
+
+import React, { useState } from "react";
+import { IoPerson } from "react-icons/io5";
+import { FaRegEyeSlash } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa";
 
 const SignUp = () => {
+  const [hidePw, setHidePw] = useState<boolean>(false);
+  const [hidePwConfirm, setHidePwConfirm] = useState<boolean>(false);
+
   return (
     <div className="w-full min-h-screen flex flex-col justify-center items-center">
       <h1 className="mt-40 mb-14 text-[30px] font-bold">PAi</h1>
-      <form className="flex flex-col justify-center">
+      <form className="md:w-8/12 flex flex-col justify-center">
         <div className="relative flex flex-col">
           <label htmlFor="nickname">닉네임</label>
           <input
             id="nickname"
             type="text"
             placeholder="영문, 한글, 숫자 2~10자"
-            className="min-w-[340px] h-10 mt-1 mb-2.5 bg-slate-200 indent-10 rounded-[10px]"
+            className="min-w-[340px] h-10 mt-1 mb-4 bg-slate-200 indent-10 rounded-[10px] focus:outline-none"
           />
-          {/* <HiOutlineMusicalNote className="absolute left-3 top-1/2 transform -translate-y-1/2" /> */}
+          <IoPerson className=" w-[18px] h-[18px] absolute left-3.5 top-1/2 transform -translate-y-1/4" />
         </div>
-        <div className="relative">
-          {" "}
+        <div className="relative flex flex-col">
           <label htmlFor="email">이메일</label>
-          <input id="email" type="email" placeholder="welcome@example.com" />
+          <input
+            id="email"
+            type="email"
+            placeholder="welcome@example.com"
+            className="min-w-[340px] h-10 mt-1 mb-4 bg-slate-200 indent-10 rounded-[10px] focus:outline-none "
+          />
         </div>
-        <div className="relative">
-          {" "}
-          <label>비밀번호</label>
-          <input placeholder="영문, 숫자, 특수문자 포함 6~12자" />
+        <div className="relative flex flex-col">
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            type={!hidePw ? "password" : "text"}
+            placeholder="영문, 숫자, 특수문자 포함 6~12자"
+            className="min-w-[340px] h-10 mt-1 mb-4 bg-slate-200 indent-10 rounded-[10px] focus:outline-none "
+          />
+          {!hidePw ? (
+            <FaRegEyeSlash
+              color="#9a9a9a"
+              className="w-[20px] h-[20px] absolute right-3.5 top-1/2 transform -translate-y-1/4 "
+              onClick={() => setHidePw(!hidePw)}
+            />
+          ) : (
+            <FaRegEye
+              color="#9a9a9a"
+              className="w-[20px] h-[20px] absolute right-3.5 top-1/2 transform -translate-y-1/4"
+              onClick={() => setHidePw(!hidePw)}
+            />
+          )}
         </div>
-        <div className="relative">
-          <label>비밀번호 확인</label>
-          <input placeholder="비밀번호 입력" />
+        <div className="relative flex flex-col">
+          <label htmlFor="confirmPassword">비밀번호 확인</label>
+          <input
+            id="confirmPassword"
+            type={!hidePwConfirm ? "password" : "text"}
+            placeholder="비밀번호 입력"
+            className="min-w-[340px] h-10 mt-1 mb-4 bg-slate-200 indent-10 rounded-[10px] focus:outline-none "
+          />
+          {!hidePwConfirm ? (
+            <FaRegEyeSlash
+              color="#9a9a9a"
+              className="w-[20px] h-[20px] absolute right-3.5 top-1/2 transform -translate-y-1/4"
+              onClick={() => setHidePwConfirm(!hidePwConfirm)}
+            />
+          ) : (
+            <FaRegEye
+              color="#9a9a9a"
+              className="w-[20px] h-[20px] absolute right-3.5 top-1/2 transform -translate-y-1/4"
+              onClick={() => setHidePwConfirm(!hidePwConfirm)}
+            />
+          )}
         </div>
-        <button>회원가입</button>
+        <button className="min-w-[340px] h-10 mt-7 mb-2.5 bg-slate-200 rounded-[10px]">회원가입</button>
       </form>
     </div>
   );
