bceb86803cbd69af8b78e520e1581c3f7971c855
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-07-30 21:20:48 +0900

    [style] 챗 페이지 css 변경

--- src/app/(main)/chat/_components/SessionBtn.tsx
+++ src/app/(main)/chat/_components/SessionBtn.tsx
@@ -2,8 +2,22 @@
 import useChatSession from "@/hooks/useChatSession";
 import { AIType } from "@/types/chat.session.type";
 
+const aiTypeConfig = {
+  assistant: {
+    name: "Pai",
+    tag: "@personal_assistant",
+    description: "저와 채팅과 음성 인식 대화로 함께 투두리스트를 만들어볼까요?"
+  },
+  friend: {
+    name: "FAi",
+    tag: "@your_friend",
+    description: "AI 친구와 고민상담, 고민과 생각을 대신 기록해 드릴게요"
+  }
+};
+
 const SessionBtn = ({ aiType }: { aiType: AIType }) => {
   const { createSession } = useChatSession(aiType);
+  const config = aiTypeConfig[aiType];
 
   const handleCreateSession = async () => {
     try {
@@ -14,11 +28,12 @@ const SessionBtn = ({ aiType }: { aiType: AIType }) => {
     }
   };
 
-  const aiTypeText = aiType === "assistant" ? "Assistant" : "Friend";
   return (
-    <>
-      <button onClick={handleCreateSession}>New {aiTypeText} Chat</button>
-    </>
+    <button onClick={handleCreateSession}>
+      <span>{config.name}</span>
+      <span>{config.tag}</span>
+      <p>{config.description}</p>
+    </button>
   );
 };
 
--- src/components/NavBarWrapper.tsx
+++ src/components/NavBarWrapper.tsx
@@ -4,7 +4,7 @@ import NavigationBar from "./NavigationBar";
 
 const NavBarWrapper = () => {
   const pathname = usePathname();
-  const hideNavPaths = ["write-diary", "diary-detail", "todo-detail", "chat", "ai-profile"];
+  const hideNavPaths = ["write-diary", "diary-detail", "todo-detail", "assistant", "friend"];
   const shouldHideNav = hideNavPaths.some((path) => pathname.includes(path));
 
   if (shouldHideNav) {
