9eae2ffa85e131e697128bf5b9dac364b0c19a1e
kimyong <right4570@naver.com>
2024-07-31 16:36:10 +0900

    제목 : 렌딩페이지 [add]: 렌딩페이지에 더미데이터 입력 및 PAi 바로가기 컴포넌트 생성 [update]: PAi시작하기 클릭 시 /todo-list로 이동기능 구현 [style]:PAi시작하기 버튼에만 임시 스타일 부여

---
+++ src/app/(landing-page)/_components/FixedActionButton.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const FixedActionButton = () => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0 h-[92px] mb-[21px] ">
+      <Link
+        href={"/todo-list"}
+        className="mobile:w-[calc(100%-32px)] mobile:mx-auto desktop:w-[300px] desktop:mx-auto h-[52px] bg-gradient-pai400-fai500-br rounded-full flex flex-row items-center"
+      >
+        {/* 폰트 굵기를 extraBold로 변경 필요하며 letter-spacing도 tailwind.config에 추가 필요 */}
+        <button className="h-[28px] mobile:w-[calc(100%-88px)] mx-auto text-system-white letter font-medium tracking-custom-letter-spacing text-base">
+          PAi 시작하기
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default FixedActionButton;
