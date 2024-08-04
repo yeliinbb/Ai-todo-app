f4e096dfd885945b168ea10ca141d2c7116189f5
kimyong <right4570@naver.com>
2024-08-04 14:58:50 +0900

    제목 : diary편집중 투두리스트 불러오기 기능 오류 수정 [bugfix]:(DiaryTextEditor.tsx)diary편집중 브라우저 새로고침이나 지도페이지이로 이동후 불러오기전의 상태로 리턴되는 문제 개선 [refactor]:(DiaryContent.tsx,DiaryList.tsx,Todolist.tsx,/diary-detail/[id]/page.tsx,,useDiary.store.ts)파일내 import사용하지않는 코드삭제 및 console.log 삭제 [refactor]:(DiaryMap.tsx)파일내부에서 선언된 타입과 카카오 지도에서 필요한 모듈 및 map type을 별도의 파일에 분리 작업진행했습니다.

--- src/app/(main)/diary/_components/DiaryList.tsx
+++ src/app/(main)/diary/_components/DiaryList.tsx
@@ -1,18 +1,15 @@
 "use client";
-import { usePathname } from "next/navigation";
 import DiaryContent from "./DiaryContent";
 import useselectedCalendarStore from "@/store/selectedCalendar.store";
 import Calendar from "@/shared/ui/Calendar";
 
 const DiaryListPage: React.FC = () => {
   const { selectedDate, setSelectedDate } = useselectedCalendarStore();
-  const pathname = usePathname();
 
   const handleDateChange = (date: Date) => {
     const formattedDate = date.toISOString().split("T")[0];
     setSelectedDate(formattedDate);
   };
-  console.log(selectedDate);
   return (
     <>
       <div className="bg-system-white">
