f0537159142fb5e9ecf975fb7bef8eb05a666c25
RyanYang99 <diddbstjd0220@naver.com>
2024-08-04 17:17:19 +0900

    [bug] : stil fixing friendchat UI not showing

--- src/app/(main)/chat/_components/FriendChat.tsx
+++ src/app/(main)/chat/_components/FriendChat.tsx
@@ -2,15 +2,14 @@
 
 import useChatSession from "@/hooks/useChatSession";
 import { CHAT_SESSIONS } from "@/lib/constants/tableNames";
-import { AIType, Message, MessageWithSaveButton } from "@/types/chat.session.type";
+import { AIType, Message } from "@/types/chat.session.type";
 import { createClient } from "@/utils/supabase/client";
 import { RealtimePostgresInsertPayload } from "@supabase/supabase-js";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import FriendMessageItem from "./FriendMessageItem";
 import ChatInput from "./ChatInput";
 import { getDateDay } from "@/lib/utils/getDateDay";
-import useChatSummary from "@/hooks/useChatSummary";
 import { queryKeys } from "@/lib/queryKeys";
 
 interface FriendChatProps {
@@ -19,15 +18,7 @@ interface FriendChatProps {
 }
 
 export type MutationContext = {
-  previousMessages: MessageWithSaveButton[] | undefined;
-};
-
-export type ServerResponse = {
-  message: MessageWithSaveButton[];
-  diaryListCompleted: boolean;
-  newDiaryItems: string[];
-  askForListChoice: boolean;
-  currentDiaryList?: string[];
+  previousMessages: Message[] | undefined;
 };
 
 const FriendChat = ({ sessionId, aiType }: FriendChatProps) => {
@@ -36,16 +27,15 @@ const FriendChat = ({ sessionId, aiType }: FriendChatProps) => {
   const textRef = useRef<HTMLInputElement>(null);
   const queryClient = useQueryClient();
   const chatContainerRef = useRef<HTMLDivElement>(null);
-  const [isDiaryMode, setIsDiaryMode] = useState(false);
-  const [currentDiaryList, setCurrentDiaryList] = useState<string[]>([]);
   const [isNewConversation, setIsNewConversation] = useState(true);
+  console.log("aiType", aiType);
 
   const {
     data: messages,
     isPending: isPendingMessages,
     isSuccess: isSuccessMessages,
     refetch: refetchMessages
-  } = useQuery<MessageWithSaveButton[]>({
+  } = useQuery<Message[]>({
     queryKey: [queryKeys.chat, aiType, sessionId],
     queryFn: async () => {
       if (!sessionId) return;
@@ -56,39 +46,41 @@ const FriendChat = ({ sessionId, aiType }: FriendChatProps) => {
 
       const data = await response.json();
       setIsNewConversation(false);
-      return data.message || []; // data.message;로 수정 끝
+      return data.message || [];
     },
     enabled: !!sessionId,
-    gcTime: 1000 * 60 * 30 // 30분 (이전의 cacheTime)
+    gcTime: 1000 * 60 * 30
   });
 
-  const sendMessageMutation = useMutation<ServerResponse, Error, string, MutationContext>({
+  const sendMessageMutation = useMutation<Message[], Error, string, MutationContext>({
     mutationFn: async (newMessage: string) => {
       const response = await fetch(`/api/chat/${aiType}/${sessionId}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify({ message: newMessage, isDiaryMode, currentDiaryList })
+        body: JSON.stringify({ message: newMessage })
       });
 
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        const errorBody = await response.text();
+        console.error("API error:", response.status, errorBody);
+        throw new Error(`Network response was not ok: ${response.status} ${errorBody}`);
       }
+
       const data = await response.json();
       setIsNewConversation(true);
       console.log("sendMessageMutation data", data);
-      return data;
+      return data.message;
     },
     onMutate: async (newMessage): Promise<MutationContext> => {
       await queryClient.cancelQueries({ queryKey: [queryKeys.chat, aiType, sessionId] });
       const previousMessages = queryClient.getQueryData<Message[]>([queryKeys.chat, aiType, sessionId]);
 
-      const userMessage: MessageWithSaveButton = {
+      const userMessage: Message = {
         role: "user" as const,
         content: newMessage,
-        created_at: new Date().toISOString(),
-        showSaveButton: false
+        created_at: new Date().toISOString()
       };
 
       queryClient.setQueryData<Message[]>([queryKeys.chat, aiType, sessionId], (oldData) => [
@@ -100,22 +92,12 @@ const FriendChat = ({ sessionId, aiType }: FriendChatProps) => {
     },
     onSuccess: (data, variables, context) => {
       console.log("onSuccess data", data);
-      queryClient.setQueryData<MessageWithSaveButton[]>([queryKeys.chat, aiType, sessionId], (oldData = []) => {
+      queryClient.setQueryData<Message[]>([queryKeys.chat, aiType, sessionId], (oldData = []) => {
         console.log("oldData", oldData);
         const withoutOptimisticUpdate = oldData.slice(0, -1);
         console.log("withoutOptimisticUpdate", withoutOptimisticUpdate);
-        return [...withoutOptimisticUpdate, ...data.message];
+        return [...withoutOptimisticUpdate, ...data];
       });
-
-      if (data.currentDiaryList) {
-        setCurrentDiaryList(data.currentDiaryList);
-      } else if (data.newDiaryItems && data.newDiaryItems.length > 0) {
-        setCurrentDiaryList((prevList) => {
-          const updatedList = [...new Set([...prevList, ...data.newDiaryItems])];
-          return updatedList;
-        });
-      }
-      setIsNewConversation(true);
     },
     onError: (error, newMessage, context) => {
       console.error("Error sending message", error);
@@ -125,57 +107,9 @@ const FriendChat = ({ sessionId, aiType }: FriendChatProps) => {
     }
   });
 
-  const { isPending } = sendMessageMutation;
-
-  const saveDiaryMutation = useMutation({
-    mutationFn: async () => {
-      const response = await fetch(`/api/chat/${aiType}/${sessionId}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ saveDiary: true, currentDiaryList })
-      });
-      if (!response.ok) {
-        throw new Error("Failed to save diary");
-      }
-      const data = await response.json();
-      console.log("saveDiaryMutation", data);
-      return data;
-    },
-    onSuccess: () => {
-      const savedMessage = {
-        role: "friend" as const,
-        content: "다이어리가 저장되었습니다.",
-        created_at: new Date().toISOString(),
-        showSaveButton: false
-      };
-      queryClient.setQueryData<MessageWithSaveButton[] | undefined>(
-        [queryKeys.chat, aiType, sessionId],
-        (oldData): MessageWithSaveButton[] | undefined => {
-          if (!oldData) return [savedMessage];
-          const updatedData = oldData.map((msg) => ({ ...msg, showSaveButton: false }));
-          return [...updatedData, savedMessage];
-        }
-      );
-      setCurrentDiaryList([]);
-      setIsDiaryMode(false);
-    },
-    onError: (error) => {
-      console.error("Error saving diary :", error);
-    }
-  });
-
   if (isSuccessMessages) {
     console.log("messages", messages);
   }
-
-  const { triggerSummary } = useChatSummary(sessionId, messages);
-  useEffect(() => {
-    if (isSuccessMessages && messages.length > 0) {
-      triggerSummary();
-    }
-  }, [messages, triggerSummary, isSuccessMessages]);
 
   useEffect(() => {
     if (chatContainerRef.current) {
@@ -231,37 +165,6 @@ const FriendChat = ({ sessionId, aiType }: FriendChatProps) => {
     }
   };
 
-  const toggleDiaryMode = async () => {
-    setIsDiaryMode((prev) => !prev);
-    if (!isDiaryMode) {
-      // 투두 모드로 전환할 때
-      const btnMessage = "일기를 작성하고 싶어";
-      await sendMessageMutation.mutateAsync(btnMessage);
-      setCurrentDiaryList([]);
-    } else {
-      const btnMessage = "일반 채팅으로 돌아갑니다.";
-      await sendMessageMutation.mutateAsync(btnMessage);
-    }
-  };
-
-  const handleCreateDiaryList = async () => {
-    setIsDiaryMode(true);
-    const btnMessage = "일기를 작성하고 싶어";
-    await sendMessageMutation.mutateAsync(btnMessage);
-    // refetchMessages();
-  };
-
-  const handleSaveButton = useCallback(() => {
-    saveDiaryMutation.mutate();
-    queryClient.setQueryData<MessageWithSaveButton[] | undefined>(
-      [queryKeys.chat, aiType, sessionId],
-      (oldData): MessageWithSaveButton[] => {
-        if (!oldData) return [];
-        return oldData.map((msg: MessageWithSaveButton) => ({ ...msg, showSaveButton: false }));
-      }
-    );
-  }, [saveDiaryMutation, queryClient, aiType, sessionId]);
-
   if (sessionIsLoading) {
     return <div>Loading session...</div>;
   }
@@ -277,8 +180,7 @@ const FriendChat = ({ sessionId, aiType }: FriendChatProps) => {
                 <FriendMessageItem
                   key={index}
                   message={message}
-                  handleSaveButton={handleSaveButton}
-                  isPending={saveDiaryMutation.isPending}
+                  isPending={sendMessageMutation.isPending}
                   isLatestAIMessage={
                     message.role === "friend" && index === messages.findLastIndex((m) => m.role === "friend")
                   }
@@ -289,12 +191,6 @@ const FriendChat = ({ sessionId, aiType }: FriendChatProps) => {
           )}
         </div>
         <div className="fixed bottom-0 left-0 right-0 p-4 rounded-t-3xl">
-          <button
-            onClick={handleCreateDiaryList}
-            className="bg-grayTrans-60080 p-5 mb-2 backdrop-blur-xl rounded-xl text-system-white w-fit text-sm leading-7 tracking-wide font-semibold"
-          >
-            {isDiaryMode ? "일반 채팅으로 돌아가기" : "일기 작성하기"}
-          </button>
           {/* 아래 공간 띄워주는 용도 div */}
           <div className="h-7"></div>
           <ChatInput
--- src/app/api/chat/friend/[id]/route.ts
+++ src/app/api/chat/friend/[id]/route.ts
@@ -1,16 +1,9 @@
-import { handleSaveChatDiary } from "@/app/api/lib/chatDiaryItemUtils";
 import { Json } from "@/types/supabase";
 import { CHAT_SESSIONS } from "@/lib/constants/tableNames";
 import openai from "@/lib/utils/chat/openaiClient";
 import { Message, MessageWithSaveButton } from "@/types/chat.session.type";
 import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
-import {
-  extractDiaryItemsFromResponse,
-  formatDiaryList,
-  getDiaryRequestType,
-  getDiarySystemMessage
-} from "@/app/api/lib/diaryPatterns";
 import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 
 export const GET = async (request: NextRequest, { params }: { params: { id: string } }) => {
@@ -30,27 +23,24 @@ export const GET = async (request: NextRequest, { params }: { params: { id: stri
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
 
-    let messages = (data[0]?.messages as Json[]) || [];
+    let messages = (data[0]?.messages as Message[]) || [];
 
     if (messages.length === 0) {
-      const welcomeMessages: MessageWithSaveButton[] = [
+      const welcomeMessages: Message[] = [
         {
           role: "friend",
           content: "안녕, 나는 너의 AI 친구 FAi야. 필요한게 있다면 뭐든 얘기해줘!",
-          created_at: new Date().toISOString(),
-          showSaveButton: false
+          created_at: new Date().toISOString()
         },
         {
           role: "friend",
           content: "일기 작성, 오늘 하루에 대한 이야기를 나눌수있어.",
-          created_at: new Date(Date.now() + 1).toISOString(),
-          showSaveButton: false
+          created_at: new Date(Date.now() + 1).toISOString()
         },
         {
           role: "friend",
           content: "일기를 작성하려면 아래 '일기 작성하기' 버튼을 눌러줘.",
-          created_at: new Date(Date.now() + 2).toISOString(),
-          showSaveButton: false
+          created_at: new Date(Date.now() + 2).toISOString()
         }
       ];
 
@@ -78,25 +68,7 @@ export const POST = async (request: NextRequest, { params }: { params: { id: str
   const supabase = createClient();
   const { id: sessionId } = params;
 
-  const { message, saveDiary, isDiaryMode, currentDiaryList } = await request.json();
-
-  if (saveDiary) {
-    try {
-      const result = await handleSaveChatDiary(supabase, sessionId);
-      if (result.success) {
-        return NextResponse.json({ message: result.message });
-      } else {
-        return NextResponse.json({ error: result.error }, { status: 400 });
-      }
-    } catch (error) {
-      return NextResponse.json({ error: "An error occurred while saving todo list." }, { status: 500 });
-    }
-  }
-
-  const diaryRequestType = getDiaryRequestType(message, isDiaryMode, currentDiaryList);
-  let showSaveButton = false;
-  let systemMessage: string;
-  let askForListChoice = false;
+  const { message } = await request.json();
 
   try {
     // 사용자 메시지 저장
@@ -117,14 +89,6 @@ export const POST = async (request: NextRequest, { params }: { params: { id: str
     const userMessage: Message = { role: "user", content: message, created_at: new Date().toISOString() };
     messages.push(userMessage);
 
-    // systemMessage 설정
-    if (isDiaryMode || diaryRequestType !== "none") {
-      systemMessage = getDiarySystemMessage(diaryRequestType, currentDiaryList);
-      console.log(systemMessage);
-    } else {
-      systemMessage = "일반적인 대화를 계속해주세요.";
-    }
-
     // Open API 호출
     const completion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
@@ -132,62 +96,21 @@ export const POST = async (request: NextRequest, { params }: { params: { id: str
         ...messages.map(
           (m): ChatCompletionMessageParam => ({ role: m.role as "system" | "user" | "assistant", content: m.content })
         ),
-        {
-          role: "system",
-          content: systemMessage
-        },
         { role: "user", content: message }
       ] as ChatCompletionMessageParam[]
     });
 
     let aiResponse = completion.choices[0].message.content;
     console.log("OpenAI API Response", completion.choices[0].message);
-    aiResponse = aiResponse ? aiResponse.replace(/^[•*]\s+/gm, "").trim() : "";
-    // console.log("aiResponse", aiResponse);
+    aiResponse = aiResponse ? aiResponse.replace(/^[•*]\s*|\d+\.\s*/gm, "").trim() : "";
+    console.log("=========================");
+    console.log("aiResponse => ", aiResponse);
 
-    let diaryItems: string[] = [];
-    let updatedDiaryList = [...currentDiaryList];
-
-    console.log("isDiaryMode => ", isDiaryMode);
-    console.log("diaryRequestType => ", diaryRequestType);
-
-    // showSaveButton 결정 및 투두 항목 정리
-    if (isDiaryMode || diaryRequestType !== "none") {
-      diaryItems = extractDiaryItemsFromResponse(aiResponse ?? "");
-      console.log("diaryItems => ", diaryItems);
-      console.log("currentDiaryList => ", currentDiaryList);
-
-      // create할 경우 새로운 투두를 추가하는것이기 때문에 기존 diaryItems 비우기
-      if (diaryRequestType === "reset") {
-        // console.log("reset");
-        updatedDiaryList = [];
-      } else if (diaryRequestType === "add") {
-        // console.log("add");
-        updatedDiaryList = [...new Set([...updatedDiaryList, ...diaryItems])];
-        showSaveButton = true; // 항목이 추가된 경우에만 save 버튼 표시
-      } else if (diaryRequestType === "list") {
-        updatedDiaryList = [...diaryItems];
-        showSaveButton = true;
-      } else if (diaryRequestType === "delete") {
-        updatedDiaryList = updatedDiaryList.filter((item) => !diaryItems.includes(item));
-        showSaveButton = true;
-      } else {
-        // 새 리스트 생성 또는 대체
-        updatedDiaryList = [...diaryItems];
-        showSaveButton = diaryItems.length > 0;
-      }
-    }
-    // 모든 케이스에 대해 formatDiaryList 적용
-    aiResponse = `${formatDiaryList(updatedDiaryList ?? [])}`;
-
-    // 기존 리스트가 있고, 새로운 항목이 추가되었을 때
-    if (currentDiaryList.length > 0 && diaryItems.some((item) => !currentDiaryList.includes(item))) {
-      askForListChoice = true;
-    } else {
-      showSaveButton = false;
-    }
-
-    const aiMessage: Message = { role: "friend", content: aiResponse ?? "", created_at: new Date().toISOString() };
+    const aiMessage: Message = {
+      role: "friend",
+      content: aiResponse ?? "",
+      created_at: new Date().toISOString()
+    };
     messages.push(aiMessage);
 
     // 업데이트 된 매시지 저장
@@ -205,37 +128,8 @@ export const POST = async (request: NextRequest, { params }: { params: { id: str
       return NextResponse.json({ error: "Failed to updated session" }, { status: 500 });
     }
 
-    const saveButtonMessage: Message = {
-      role: "friend",
-      content: "작성된 일기 내용을 일기에 추가하고 싶다면 '저장하기' 버튼을 눌러주세요",
-      created_at: new Date().toISOString()
-    };
-
-    const choiceMessage: Message = {
-      role: "system",
-      content: "새로운 일기내용을 오늘 일기에 추가하시겠습니까, 아니면 새로운 일기로 대체하시겠습니까?",
-      created_at: new Date().toISOString()
-    };
-
-    const diaryListCompleted = aiResponse?.includes("일기 작성이 완료되었습니다.");
-    const hasNewDiaryItems =
-      diaryItems.length > 0 && diaryItems.some((item: string) => !currentDiaryList.includes(item));
-
-    // console.log("showSaveButton", showSaveButton);
-    // console.log("askForListChoice", askForListChoice);
-    console.log("updatedDiaryList => ", updatedDiaryList);
-
     return NextResponse.json({
-      message: [
-        { ...userMessage },
-        { ...aiMessage },
-        // askForListChoice ? choiceMessage : null,
-        showSaveButton ? { ...saveButtonMessage, showSaveButton } : null
-      ].filter(Boolean),
-      diaryListCompleted,
-      newDiaryItems: hasNewDiaryItems ? diaryItems : [],
-      currentDiaryList: updatedDiaryList,
-      askForListChoice
+      message: [{ ...userMessage }, { ...aiMessage }].filter(Boolean)
     });
   } catch (error) {
     console.error("Error:", error);
