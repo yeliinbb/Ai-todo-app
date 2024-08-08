359c9c9f1eb2e031517fca89636cf9bbd91551d5
kimyong <right4570@naver.com>
2024-08-08 15:21:14 +0900

    제목: diary관련 파일 css작업 [update]:diary map ui 수정 및 장소명 표시 기능 구현/기존infowindow대신 커스텀텀infowindow로 대체

--- src/app/(main)/diary/_components/DiaryList.tsx
+++ src/app/(main)/diary/_components/DiaryList.tsx
@@ -6,10 +6,12 @@ import Calendar from "@/shared/ui/Calendar";
 const DiaryListPage: React.FC = () => {
   const { selectedDate, setSelectedDate } = useselectedCalendarStore();
 
+
   const handleDateChange = (date: Date) => {
     const formattedDate = date.toISOString().split("T")[0];
     setSelectedDate(formattedDate);
   };
+
   return (
     <>
       <div className="flex flex-col h-[calc(100vh-4.5rem)]">
@@ -19,7 +21,6 @@ const DiaryListPage: React.FC = () => {
         <div className="flex-grow overflow-auto bg-faiTrans-20060 rounded-t-[48px]">
           <DiaryContent date={selectedDate} />
         </div>
-
       </div>
     </>
   );
