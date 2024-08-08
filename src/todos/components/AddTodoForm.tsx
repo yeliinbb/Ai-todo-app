98a5b5bdd6d2ce408e5e5ff7cb0cdfb3c3304384
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-08-08 12:17:42 +0900

    [fix] 충돌 해결

--- src/todos/components/AddTodoForm.tsx
+++ src/todos/components/AddTodoForm.tsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import TimeSelect from "@/shared/TimeSelect";
-import { useUserData } from "@/hooks/useUserData";
-import { IoCheckmarkCircleOutline, IoLocationOutline, IoReaderOutline, IoTimeOutline } from "react-icons/io5";
-import { ToastContainer, toast } from "react-toastify";
+import dayjs from "dayjs";
 
-export type TodoFormData = {
+export type AddTodoFormData = {
   title: string;
   description: string;
   eventTime: [number, number] | null;
@@ -14,77 +12,66 @@ export type TodoFormData = {
   } | null;
 };
 export interface AddTodoFormProps {
-  onSubmit?: (data: TodoFormData) => void;
+  onSubmit?: (data: AddTodoFormData) => void;
+  selectedDate: Date;
 }
 
-const AddTodoForm = ({ onSubmit }: AddTodoFormProps) => {
-  const [formData, setFormData] = useState<TodoFormData>({
+const AddTodoForm = ({ onSubmit, selectedDate }: AddTodoFormProps) => {
+  const [formData, setFormData] = useState<AddTodoFormData>({
     title: "",
     description: "",
-    eventTime: null,
+    eventTime: [0, 0],
     address: null
   });
 
-  const { data } = useUserData();
-  const userId = data?.user_id;
-
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!formData.title) return toast.warn("투두를 입력해주세요.");
     onSubmit?.(formData);
     setFormData({
       title: "",
       description: "",
-      eventTime: null,
+      eventTime: [0, 0],
       address: null
     });
   };
 
   return (
-    <div className="relative min-h-screen flex flex-col items-center">
-      <form onSubmit={handleSubmit} className="flex flex-col items-center w-full max-w-md">
-        <ul className="flex flex-col gap-4 w-full px-4">
-          <li className="flex items-center border-b-gray-400 w-full h-8 justify-center">
-            <IoCheckmarkCircleOutline className="text-pai-400 w-[18.3px] h-[18.3px] mr-3" />
+    <div>
+      <div>{dayjs(selectedDate).format("YYYY년 M월 D일 ddd요일")}</div>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          <li>
             <input
               type="text"
               placeholder="제목을 입력해주세요."
               value={formData.title}
               onChange={(e) => setFormData((prev) => ({ ...prev, title: e.target.value }))}
-              className="flex-1"
             />
           </li>
-          <li className="flex items-center w-full h-8 justify-center">
-            <IoReaderOutline className="w-5 h-5 text-gray-700 mr-3" />
-            <input
+          <li>
+            <textarea
               placeholder="메모"
               value={formData.description}
               onChange={(e) => setFormData((prev) => ({ ...prev, description: e.target.value }))}
-              className="flex-1"
             />
           </li>
-          <li className="flex items-center w-full h-8 justify-center">
-            <IoTimeOutline className="w-5 h-5 text-gray-700 mr-3" />
-            <div className="flex-1">
-              <TimeSelect
-                value={formData.eventTime ?? undefined}
-                onChange={(value) => setFormData((prev) => ({ ...prev, eventTime: value ?? null }))}
-              />
-            </div>
+          <li>
+            <TimeSelect
+              value={formData.eventTime ?? undefined}
+              onChange={(value) => setFormData((prev) => ({ ...prev, eventTime: value ?? null }))}
+            />
           </li>
-          <li className="flex items-center w-full h-8 justify-center">
-            <IoLocationOutline className="w-5 h-5 text-gray-700 mr-3" />
-            <button type="button" className="text-gray-400 flex-1 text-left">
-              장소 선택
-            </button>
+          <li>
+            <span>장소 선택</span>
           </li>
+          {/* <li>
+            <span>5분 전</span>
+            <span>10분 전</span>
+            <span>15분 전</span>
+            <span>30분 전</span>
+          </li> */}
         </ul>
-        <button
-          type="submit"
-          className="w-[calc(100%-32px)] h-11 bg-pai-400 text-system-white rounded-[24px] my-4 mx-auto"
-        >
-          추가하기
-        </button>
+        <button type="submit">추가하기</button>
       </form>
     </div>
   );
