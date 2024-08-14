e01e662585895d28e0383a11cba19700cb9d4a79
doeheekim <kimamy20@gmail.com>
2024-08-15 02:08:33 +0900

    [bugfix] : pre/next month daycell에도 투두가 있는 경우의 스타일이 적용되는 버그 수정, 오늘 날짜를 선택 시 오늘 뱃지 위치가 미세하게 이동하는 버그 수정, 폰트/사이즈 스타일 추가

--- src/shared/ui/Calendar/CalendarDayContent.tsx
+++ src/shared/ui/Calendar/CalendarDayContent.tsx
@@ -5,18 +5,18 @@ import dayjs from "dayjs";
 export type CalenderColors = "pai" | "fai";
 const colorThemes = {
   pai: {
-    thisMonth: "bg-grayTrans-20032 text-gray-400",
+    thisMonth: "bg-gray-200 text-gray-400",
     otherMonth: "text-gray-200",
-    existEvents: "bg-pai-200 text-gray-600",
-    allDoneEvents: "bg-pai-400 text-white",
-    selected: "border-pai-600 border-2"
+    existEvents: "bg-pai-200 text-gray-900",
+    allDoneEvents: "bg-pai-400 text-system-white",
+    selected: "outline outline-pai-400 outline-[0.13rem]"
   },
   fai: {
-    thisMonth: "bg-grayTrans-20032 text-gray-400",
+    thisMonth: "bg-gray-200 text-gray-400",
     otherMonth: "text-gray-200",
-    existEvents: "bg-fai-200 text-gray-600",
-    allDoneEvents: "bg-fai-400 text-white",
-    selected: "border-fai-600 border-2"
+    existEvents: "bg-fai-200  text-gray-600",
+    allDoneEvents: "bg-fai-400 text-system-white",
+    selected: "outline outline-fai-600 outline-[0.13rem]"
   }
 };
 
@@ -44,6 +44,10 @@ const CalendarDayContent = ({
   }, [events, date]);
 
   const colorClassNames = useMemo(() => {
+    // 다른 달
+    if (!isCurrentMonth) {
+      return colorThemes[color]?.otherMonth;
+    }
     // 투두가 있을 때
     if (eventsInDay.length > 0) {
       // 모두 완료
@@ -53,21 +57,17 @@ const CalendarDayContent = ({
       // 미완료
       return colorThemes[color]?.existEvents;
     }
-    // 이번달
-    if (isCurrentMonth) {
-      return colorThemes[color]?.thisMonth;
-    }
-    return colorThemes[color]?.otherMonth;
+    return colorThemes[color]?.thisMonth;
   }, [isCurrentMonth, eventsInDay, color]);
 
   return (
     <div
-      className={`flex items-center relative justify-center p-1 w-[36px] h-[36px] rounded-full my-[12px] ${colorClassNames} ${
+      className={`font-sans text-h7 font-extrabold flex items-center relative justify-center p-1 w-9 h-9 rounded-full my-3 ${colorClassNames} ${
         isSelected ? colorThemes[color]?.selected : ""
       }`}
     >
       {isToday && (
-        <div className="absolute -top-[10px] bg-system-black text-system-white text-center w-[36px] rounded-xl py-1 text-[10px]">
+        <div className="text-c1 text-center absolute -top-[0.75rem] bg-gray-900 text-system-white rounded-[0.75rem] w-[2.25rem] h-[1.25rem] flex items-center justify-center">
           오늘
         </div>
       )}
