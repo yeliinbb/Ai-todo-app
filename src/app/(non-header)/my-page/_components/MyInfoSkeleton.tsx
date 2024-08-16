01ca9f004d08b19b7f2ca0ca90179c191032d94c
oneieo <sun020716@gmail.com>
2024-08-16 11:31:47 +0900

    [style] : (NonLoggedIn.tsx) 웹 반응형

--- src/app/(non-header)/my-page/_components/MyInfoSkeleton.tsx
+++ src/app/(non-header)/my-page/_components/MyInfoSkeleton.tsx
@@ -20,7 +20,7 @@ const MyInfoSkeleton = () => {
             style={{ boxShadow: "0px 0px 8px 0px rgba(0, 0, 0, 0.16)" }}
             className="w-full min-h-[480px] max-w-[390px] mt-5 pt-5 px-4 rounded-t-[48px] bg-system-white h-full"
           >
-            <h1 className="w-[343px] h-7 flex items-center pl-3 text-gray-300 font-extrabold text-base ">설정</h1>
+            <h1 className="w-[343px] h-7 flex items-center pl-3 text-gray-300 font-extrabold text-base">설정</h1>
             <ul>
               <li
                 className={`relative min-w-[343px] h-16 flex items-center px-3 py-5 border-b-[1px] border-gray-100 text-gray-300 `}
@@ -29,27 +29,27 @@ const MyInfoSkeleton = () => {
                 <p className="flex items-center h-[28px] ml-1 text-gray-300  font-medium text-base">이메일 계정</p>
                 <p className="absolute right-2 text-gray-300 font-medium text-base"></p>
               </li>
-              <li className="relative min-w-[343px] h-16 flex items-center px-3 py-5 border-b-[1px] border-gray-100 duration-200 text-gray-300 hover:cursor-pointer hover:bg-gray-100 active:bg-gray-200">
+              <li className="relative min-w-[343px] h-16 flex items-center px-3 py-5 border-b-[1px] border-gray-100 duration-200 text-gray-300">
                 <NicknameSmall />
                 <p className="flex items-center h-[28px] ml-1 text-gray-300  font-medium text-base">닉네임 변경</p>
                 <div className="absolute right-2">
                   <NextBtn />
                 </div>
               </li>
-              <li className="relative min-w-[343px] h-16 flex items-center px-3 py-5 border-b-[1px]  border-gray-100 duration-200 text-gray-300 hover:cursor-pointer hover:bg-gray-100 active:bg-gray-200">
+              <li className="relative min-w-[343px] h-16 flex items-center px-3 py-5 border-b-[1px]  border-gray-100 duration-200 text-gray-300">
                 <PasswordSmall />
                 <p className="flex items-center h-[28px] ml-1 text-gray-300  font-medium text-base">비밀번호 변경</p>
                 <div className="absolute right-2">
                   <NextBtn />
                 </div>
               </li>
-              <li className="relative min-w-[343px] h-16 mt-5 flex items-center px-3 py-5 border-b-[1px]  border-gray-100 duration-200 text-gray-800 font-medium text-base hover:cursor-pointer hover:bg-gray-100 active:bg-gray-200">
+              <li className="relative min-w-[343px] h-16 mt-5 flex items-center px-3 py-5 border-b-[1px]  border-gray-100 duration-200 text-gray-800 font-medium text-base">
                 <p className="text-gray-300  font-medium text-base">로그아웃</p>
                 <div className="absolute right-2">
                   <NextBtn />
                 </div>
               </li>
-              <li className="relative min-w-[343px] h-16 flex items-center px-3 py-5 border-b-[1px]  border-gray-100 duration-200 text-gray-800 font-medium text-base hover:cursor-pointer hover:bg-gray-100 active:bg-gray-200">
+              <li className="relative min-w-[343px] h-16 flex items-center px-3 py-5 border-b-[1px]  border-gray-100 duration-200 text-gray-800 font-medium text-base">
                 <p className="text-gray-300  font-medium text-base">회원탈퇴</p>
                 <div className="absolute right-2">
                   <NextBtn />
