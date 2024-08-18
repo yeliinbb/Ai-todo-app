52281961ec56c41d4f8ebcc813f7b45b746fdef0
oneieo <sun020716@gmail.com>
2024-08-18 22:55:49 +0900

    [style] : (PAiCard.tsx) (FAiCard.tsx) 파이 카드 컴포넌트 추가

--- src/app/(main)/home/_components/Home.tsx
+++ src/app/(main)/home/_components/Home.tsx
@@ -1,5 +1,53 @@
+import Image from "next/image";
+import PAiCard from "./PAiCard";
+import FAiCard from "./FAiCard";
+
 const Home = () => {
-  return <div className="pt-[4.5rem]">홈페이지</div>;
+  return (
+    <div className="w-full h-full flex flex-col items-center pt-[4.5rem]">
+      <Image
+        src={"/bannerHome-Mobile.png"}
+        width={375}
+        height={160}
+        alt="PAi와 함께하는 나의 일상 기록"
+        priority
+        sizes="(max-width: 1200px) 100vw, 375px"
+        className="block desktop:hidden w-full h-auto"
+      />
+      <Image
+        src={"/bannerHome-PC.png"}
+        width={1284}
+        height={320}
+        alt="PAi와 함께하는 나의 일상 기록"
+        priority
+        className="hidden desktop:block"
+      />
+      <h1 className="desktop:text-sh1 desktop:my-10 my-5 text-center text-sh4 text-transparent bg-clip-text bg-gradient-pai400-fai500-br">
+        오늘은 어떤 기록을 함께 할까요?
+      </h1>
+      <div className="desktop:gap-[2.5rem] flex justify-center gap-[0.563rem] w-[calc(100%-32px)]">
+        <PAiCard />
+        <FAiCard />
+      </div>
+      <Image
+        src={"/bannerHome2-Mobile.png"}
+        width={343}
+        height={106}
+        alt="홈에서도 가능한 투두와 다이어리 작성!"
+        priority
+        sizes="(max-width: 1200px) 100vw, 343px"
+        className="desktop:hidden block mt-5 mb-[6rem] w-[calc(100%-32px)] h-auto"
+      />
+      <Image
+        src={"/bannerHome2-PC.png"}
+        width={1180}
+        height={160}
+        alt="홈에서도 가능한 투두와 다이어리 작성!"
+        priority
+        className="desktop:mb-[7.25rem] hidden mt-12 desktop:block mb-[6rem]"
+      />
+    </div>
+  );
 };
 
 export default Home;
--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from "next";
 import Home from "./_components/Home";
 
-const metadata: Metadata = {
-  title: "PAi 홈페이지",
+export const metadata: Metadata = {
+  title: "Home",
   description: "PAi 홈페이지입니다.",
   keywords: ["chat", "assistant", "friend", "todo", "todolist", "diary", "투두", "다이어리"],
   openGraph: {
