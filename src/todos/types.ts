f33bf6d839152838e439e4328f4d354868a33ce2
doeheekim <kimamy20@gmail.com>
2024-08-18 22:44:00 +0900

    [update] : todos 테이블 컬럼별 타입 재정의, address 관련 데이터 및 타입 추가

--- src/todos/types.ts
+++ src/todos/types.ts
@@ -1,3 +1,15 @@
+import { LocationData } from "@/shared/LocationSelect/types";
 import { Tables } from "../types/supabase";
 
-export type Todo = Tables<"todos">;
+export type TodoData = Tables<"todos">;
+
+export type Todo = {
+  todo_id: string;
+  todo_title: string;
+  todo_description?: string;
+  created_at: string;
+  event_datetime?: string;
+  is_done: boolean;
+  is_all_day_event: boolean;
+  address: LocationData;
+};
