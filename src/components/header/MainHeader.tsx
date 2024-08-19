d15c34a3556945d1a83f1387be163966f5ae1904
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-08-19 11:58:41 +0900

    [fix] 헤더 로직 수정

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -7,6 +7,7 @@ import CloseIcon from "../../assets/x.svg";
 import useSideNavStore from "@/store/useSideNavStore";
 import CommonBtn from "../CommonBtn";
 import Logo from "../Logo";
+import usePageCheck from "@/hooks/usePageCheck";
 
 interface HeaderProps {
   className?: string;
@@ -16,7 +17,7 @@ const MainHeader = ({ className = "" }: HeaderProps) => {
   const pathname = usePathname();
   const router = useRouter();
   const { toggleSideNav } = useSideNavStore();
-
+  const { isHomePage, isLoginPage, isChatPage, isTodoPage, isDiaryPage, isPaiPage, isFaiPage } = usePageCheck();
   const hideHeaderPaths = ["write-diary", "diary-detail", "todo-detail", "diary-map"];
   const shouldHideHeader = hideHeaderPaths.some((path) => pathname.includes(path));
 
@@ -24,29 +25,23 @@ const MainHeader = ({ className = "" }: HeaderProps) => {
     return null;
   }
 
-  const isDiaryPage = pathname === "/diary";
-  const isChatPage = pathname.includes("/chat");
-  const isTodoPage = pathname === "/todo-list";
-  const isChatMainPage = pathname === "/chat";
-  const isFai = pathname.includes("/friend");
+  // const isDiaryPage = pathname === "/diary";
+  // const isChatPage = pathname.includes("/chat");
+  // const isTodoPage = pathname === "/todo-list";
+  // const isChatMainPage = pathname === "/chat";
+  // const isFai = pathname.includes("/friend");
 
   const navigateToMyPage = () => router.push("/my-page");
 
   const getLeftButton = () => {
-    if (isDiaryPage) {
-      return <Logo />;
-    }
     if (isChatPage || isTodoPage) {
       return <CommonBtn icon={<SearchIcon />} onClick={toggleSideNav} />;
     }
-    return null; // 또는 다른 기본 버튼
+    return <Logo />; // 또는 다른 기본 버튼
   };
 
   const getRightButton = () => {
-    if (isTodoPage || isChatMainPage) {
-      return <CommonBtn icon={<UserIcon />} onClick={navigateToMyPage} />;
-    }
-    if (isChatPage) {
+    if (isPaiPage || isFaiPage) {
       return (
         <Link href="/chat">
           <CommonBtn icon={<CloseIcon />} />
@@ -61,7 +56,7 @@ const MainHeader = ({ className = "" }: HeaderProps) => {
       className={`absolute top-0 left-0 right-0 z-10 flex flex-shrink-0 justify-between items-center h-[4.5rem] mobile:px-4 pt-2 pb-5 bg-header-gradient desktop:px-14 ${className}`}
     >
       {getLeftButton()}
-      {!isDiaryPage && (isTodoPage || isChatMainPage ? <Logo /> : <Logo isFai={isFai} />)}
+      {!isDiaryPage && (isTodoPage || isChatPage ? <Logo /> : <Logo isFai={isFaiPage} />)}
       {getRightButton()}
     </div>
   );
