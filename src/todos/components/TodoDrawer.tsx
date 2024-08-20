afaa9084f915e0147ab08f09790ac0542bfb472f
doeheekim <kimamy20@gmail.com>
2024-08-20 19:24:55 +0900

    [fix] : 투두 카드 내 정렬, 기타 ui 수정

--- src/todos/components/TodoDrawer.tsx
+++ src/todos/components/TodoDrawer.tsx
@@ -18,7 +18,7 @@ const TodoDrawer = ({ open, onClose, selectedDate, children, className, modal }:
         onPointerDownOutside={onClose}
         className={cn(
           "h-[calc(100svh-4.625rem)] px-4 pb-5 transition-all",
-          "desktop:h-auto desktop:max-w-[600px] desktop:w-[50%] desktop:max-h-[85vh] desktop:left-1/2 desktop:top-1/2 desktop:-translate-x-1/2 desktop:-translate-y-1/2",
+          "desktop:h-[calc(100svh-4.625rem)] desktop:w-[calc(21.75rem+(100vw-1200px)*0.675] desktop:left-1/3 desktop:-translate-x-1/2 desktop:-translate-y-1/2",
           className
         )}
       >
