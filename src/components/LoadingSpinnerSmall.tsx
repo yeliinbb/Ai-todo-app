557566f312e7b1453561f397088017b71e4838e1
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-08-20 22:20:31 +0900

    검색 반응형 ui 수정

--- src/components/LoadingSpinnerSmall.tsx
+++ src/components/LoadingSpinnerSmall.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 
 const LoadingSpinnerSmall = () => {
-  return <Image alt="로딩 중" src="/LoadingSpinner.gif" width={25} height={25} priority />;
+  return (
+    <div>
+      <Image alt="로딩 중" src="/LoadingSpinner.gif" width={25} height={25} priority />
+    </div>
+  );
 };
 
 export default LoadingSpinnerSmall;
