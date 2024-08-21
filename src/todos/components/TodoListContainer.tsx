6816001d54650f018a3635cec48ca25f4f0b28f0
doeheekim <kimamy20@gmail.com>
2024-08-21 12:23:28 +0900

    [fix] : chore

--- src/todos/components/TodoListContainer.tsx
+++ src/todos/components/TodoListContainer.tsx
@@ -116,12 +116,12 @@ const TodoListContainer = ({ todos, selectedDate, onSubmit }: TodoListContainerP
       <div className="flex flex-col w-full h-full pb-[100px] px-4 gap-[1.25rem] max-w-[39.3rem] desktop:gap-[2rem] desktop:p-0">
         {isDesktop ? (
           <>
-            <div className="flex flex-col w-full h-full pb-[100px] px-4 gap-[1.25rem] max-w-[39.3rem] desktop:gap-[2rem] desktop:p-0 rounded-t-[5.63rem] bg-system-white">
+            <div className="flex flex-col w-full h-full pb-[100px] px-4 gap-[1.25rem] max-w-[39.3rem] desktop:gap-[2rem] desktop:p-0 rounded-t-[5.63rem] bg-system-white bg-gradient-to-b">
               <div className="flex flex-col items-start self-stretch h-full">
                 <div className="w-full flex flex-col items-center gap-[0.625rem] px-[3.25rem] py-[1.75rem]">
                   <p className="text-bc2 text-pai-900">{dayjs(selectedDate).format("YYYY년 M월 D일 ddd요일")}</p>
                 </div>
-                <div className="w-full bg-system-white flex flex-col gap-[2rem] px-[2.75rem] py-[2rem] pb-[78px] overflow-y-auto scrollbar-hide scrollbar-smooth">
+                <div className="w-full bg-gradient-to-b flex flex-col gap-[2rem] px-[2.75rem] py-[2rem] pb-[78px] overflow-y-auto scrollbar-hide scrollbar-smooth">
                   {TodoListNode}
                 </div>
               </div>
