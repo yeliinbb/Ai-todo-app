d41ff4120536cd0597fe7fea4e1982a4c2e49f2c
doeheekim <kimamy20@gmail.com>
2024-08-21 11:18:54 +0900

    [fix] : 캘린더 collapsed 이후에도 현재 년도&달로 이동하지 않게 수정

--- src/shared/ui/Calendar/index.tsx
+++ src/shared/ui/Calendar/index.tsx
@@ -86,12 +86,12 @@ const Calendar = ({ selectedDate, onChange, events, initialCollapsed, color, cla
             <div className="w-[2.5rem] flex items-center justify-start">
               <button
                 onClick={() => {
-                  // const willCollapsed = !collapsed;
-                  // if (willCollapsed) {
-                  //   changeYear(today.year());
-                  //   changeMonth(today.month());
-                  // }
-                  setCollapsed((prev) => !prev);
+                  const willCollapsed = !collapsed;
+                  if (willCollapsed) {
+                    changeYear(dayjs(monthDate).year());
+                    changeMonth(dayjs(monthDate).month());
+                  }
+                  setCollapsed(willCollapsed);
                 }}
               >
                 {collapsed ? <MonthlyCalendarIcon /> : <WeeklyCalendarIcon />}
