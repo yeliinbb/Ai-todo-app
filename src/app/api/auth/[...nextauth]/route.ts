612133c88ecdb2d1df70f00b34200cfe5f5a35f1
oneieo <sun020716@gmail.com>
2024-09-19 15:49:02 +0900

    [fix] : next-auth 라우트 파일 주석처리

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,16 @@
-import NextAuth from "next-auth/next";
-import NaverProvider from "next-auth/providers/naver";
-import { NextAuthOptions } from "next-auth";
+// import NextAuth from "next-auth/next";
+// import NaverProvider from "next-auth/providers/naver";
+// import { NextAuthOptions } from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID!,
-      clientSecret: process.env.NAVER_CLIENT_SECRET!
-    })
-  ]
-};
+// export const authOptions: NextAuthOptions = {
+//   providers: [
+//     NaverProvider({
+//       clientId: process.env.NAVER_CLIENT_ID!,
+//       clientSecret: process.env.NAVER_CLIENT_SECRET!
+//     })
+//   ]
+// };
 
-const handler = NextAuth(authOptions);
+// const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
